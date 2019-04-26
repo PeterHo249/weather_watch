704b22e86805cbcc180641313643813c734ec5fd
PETERHO\PeterHo <peter_ho_249@outlook.com>
2019-04-26 10:03:27 +0700

    add images

--- lib/data_provider/api_config.dart
+++ lib/data_provider/api_config.dart
@@ -1,2 +1,8 @@
+import 'package:darksky_weather/darksky_weather_io.dart';
+
 String apiSecretKey = '5c3667d552d7ea37b2e5147cf1430cce';
-String apiDomain = 'https://api.darksky.net/forecast';
+
+DarkSkyWeather darkSkyWeather = DarkSkyWeather(
+  apiSecretKey,
+  units: Units.SI,
+);
